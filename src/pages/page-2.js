4ff6bcd0761db1b3a2c2eb930b1e26d11da19a90
Abhinav Varghese Abraham <abhinavthottunkal4213@gmail.com>
2021-12-04 15:05:20 +0530

    second commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -9,8 +9,17 @@ const SecondPage = () => (
     <Seo title="Page two" />
     <h1>THE TWO KINDS OF INDIAN STUDENTS</h1>
     <br></br>
-    <p><b>THIS PAGE IS UNDER THE WATER, PLS HALP</b></p>
-    
+    <p><i>This page is still under development, sorry for the inconvenience.</i></p>
+    <br></br>
+    <StaticImage
+      src="https://cdn.discordapp.com/attachments/916339247059959889/916619594628726804/unknown.png"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+  
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
